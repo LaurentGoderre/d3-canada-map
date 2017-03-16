8dcd0e10be054d82e653fdaaa32849aca7baf5f0
Laurent Goderre <laurent.goderre@gmail.com>
2017-03-16 10:42:15 -0400

    Updated built files

--- dest/d3-canada-map.js
+++ dest/d3-canada-map.js
@@ -26,6 +26,48 @@ this.getCanadaMap = function(svg, settings) {
 					dispatch.call("zoom", this, province);
 				});
 		},
+		baseMapCallback = function(error, canada) {
+			if (error) {
+				if (error instanceof Error) {
+					return dispatch.call("error", rtnObj, error);
+				} else if (error instanceof ProgressEvent) {
+					return dispatch.call("error", rtnObj, new Error("Could not load the map file (" + error.target.status + " " + error.target.statusText + ")"));
+				}
+			}
+			try {
+				var canadaLayer = svg.append("g")
+						.attr("class", "canada-map"),
+					provincesKeys = Object.keys(canada.objects),
+					province, provinceShort, p;
+
+				for(p = 0; p < provincesKeys.length; p += 1) {
+					province = provincesKeys[p];
+					provinceShort = province.substr(3);
+
+					//Filter provinces based on specified argument
+					if (!settings.provinces || settings.provinces.indexOf(provinceShort) !== -1) {
+						canadaLayer.append("path")
+							.datum(topojson.feature(canada, canada.objects[province]))
+							.attr("class", provinceShort)
+							.attr("d", path);
+
+						rtnObj.provinces[provinceShort] = {
+							zoom: zoom.bind(rtnObj, provinceShort)
+						};
+					}
+				}
+
+				rtnObj.obj = canadaLayer;
+				rtnObj._bBox = canadaLayer.node().getBBox();
+				rtnObj.zoom = zoom.bind(rtnObj);
+
+				rtnObj.zoom();
+
+				dispatch.call("loaded", rtnObj);
+			} catch(e) {
+				return dispatch.call("error", rtnObj, e);
+			}
+		},
 		rtnObj, projection, path;
 
 	settings = settings || {};
@@ -44,54 +86,21 @@ this.getCanadaMap = function(svg, settings) {
 		settings.provinces = [settings.provinces];
 	}
 
+	if (!settings.baseMap) {
+		settings.baseMap = "data/canada.json";
+	}
+
 	projection = settings.projection = settings.projection ||
 		d3.geoTransverseMercator()
 			.rotate([95,0]);
 	path = d3.geoPath()
 		.projection(projection);
 
-	d3.json("/data/canada.json", function(error, canada) {
-		if (error) {
-			if (error instanceof Error) {
-				return dispatch.call("error", rtnObj, error);
-			} else if (error instanceof ProgressEvent) {
-				return dispatch.call("error", rtnObj, new Error("Could not load the map file (" + error.target.status + " " + error.target.statusText + ")"));
-			}
-		}
-		try {
-			var canadaLayer = svg.append("g")
-					.attr("class", "canada-map"),
-				provincesKeys = Object.keys(canada.objects),
-				province, provinceShort, p;
-
-			for(p = 0; p < provincesKeys.length; p += 1) {
-				province = provincesKeys[p];
-				provinceShort = province.substr(3);
-
-				//Filter provinces based on specified argument
-				if (!settings.provinces || settings.provinces.indexOf(provinceShort) !== -1) {
-					canadaLayer.append("path")
-						.datum(topojson.feature(canada, canada.objects[province]))
-						.attr("class", provinceShort)
-						.attr("d", path);
-
-					rtnObj.provinces[provinceShort] = {
-						zoom: zoom.bind(rtnObj, provinceShort)
-					};
-				}
-			}
-
-			rtnObj.obj = canadaLayer;
-			rtnObj._bBox = canadaLayer.node().getBBox();
-			rtnObj.zoom = zoom.bind(rtnObj);
-
-			rtnObj.zoom();
-
-			dispatch.call("loaded", rtnObj);
-		} catch(e) {
-			return dispatch.call("error", rtnObj, e);
-		}
-	});
+	if (typeof settings.baseMap === "object") {
+		baseMapCallback(null, settings.baseMap);
+	} else {
+		d3.json(settings.baseMap, baseMapCallback);
+	}
 
 	return rtnObj;
 };
